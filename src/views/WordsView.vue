05ee57c6caed3b2346b680e9f3f9a227b74068af
Dasha <dasha@100up.ru>
2022-11-15 13:12:13 +0300

    add modals and filter

--- src/views/WordsView.vue
+++ src/views/WordsView.vue
@@ -14,9 +14,12 @@
         </b-row>
         <b-button variant="primary" class="mt-3" @click="findWords()">Поиск</b-button>
 
-        <b-list-group>
-            <b-list-group-item v-for="word in currentWords" :key="word.id">{{word.name}}</b-list-group-item>
-        </b-list-group>
+        <div class="result-block">
+            <div class="result-block__list" v-if="!isNotFound">
+                <b-button class="mt-1" variant="outline-secondary" v-for="word in currentWords" :key="word.id" @click="showInfo(word)">{{word.name}}</b-button>
+            </div>
+            <b-alert v-else show variant="info">Ничего не найдено</b-alert>
+        </div>
     </div>
 </template>
 
@@ -28,38 +31,107 @@
             return{
                 currentWords: null,
                 letterCount: 0,
+                model: '',
+                isNotFound: false
             }
         },
         methods: {
             findWords() {
-               // this.currentLetters.splice(0,this.currentLetters.length);
-                console.log(this.currentLetters)
-                this.currentWords = words.filter(el => el.name.length === this.letterCount)
-                console.log(this.currentWords.filter(elem => elem.name.indexOf(this.currentLetters) !== -1))
+                this.currentWords = words.filter(el => el.name.length === this.letterCount);
+                if (this.currentLetters.every(el => el === undefined)) return;
+                let reg = '';
+                this.currentLetters.forEach(letter => {
+                    if (letter === undefined) {
+                        reg += '.'
+                    } else {
+                        reg += letter
+                    }
+                });
+                this.currentWords = this.currentWords.filter(elem => elem.name.match(reg));
+                this.isNotFound = this.currentWords.length < 1;
+            },
+            showInfo(word){
+                console.log(word.similars);
+                this.model = '';
+                const synonymsTitle = {
+                        name: "Синонимы",
+                        result: null
+                    },
+                    antonymsTitle = {
+                        name: "Антонимы",
+                        result: null
+                    },
+                    similarsTitle = {
+                        name: "Похожие",
+                        result: null
+                    };
+                const elem = this.$createElement;
+
+                const parametersHandler = function(parameters, title) {
+                    const titleClass = typeof parameters === 'undefined' ? 'text-danger' : 'text-success';
+                    title.result = elem('h5', {class: [titleClass]}, [title.name]);
+                    if (typeof parameters === 'undefined') return 'Не найдены';
+                    return parameters.join(', ');
+                }
+
+                const synonyms = parametersHandler(word.synonyms, synonymsTitle);
+                const antonyms = parametersHandler(word.antonyms, antonymsTitle);
+                const similars = parametersHandler(word.similars, similarsTitle);
+
+                const body = elem('div', {}, [
+                    synonymsTitle.result,
+                    elem('p', {}, [synonyms]),
+                    antonymsTitle.result,
+                    elem('p', {}, [antonyms]),
+                    similarsTitle.result,
+                    elem('p', {}, [similars])
+                    ]);
+
+
+                this.$bvModal.msgBoxOk([body], {
+                    title: `Слово - ${word.name}`,
+                    size: 'lg',
+                    buttonSize: 'sm',
+                    okVariant: 'success',
+                    centered: true
+                })
+                    .then(value => {
+                        this.modal = value
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
             }
         },
         computed: {
             currentLetters(){
                 return Array.from({length: this.letterCount})
             }
-            // inputState() {
-            //     return this.name.length > 2 ? true : false
-            // }
         },
     }
 </script>
 
 <style scoped>
-.form-control{
-    text-align: center;
-    padding: 5px;
-}
-.row{
-    margin-left: -4px;
-    margin-right: -4px;
-}
-.col-1{
-    padding-left: 4px;
-    padding-right: 4px;
-}
+    .form-control{
+        text-align: center;
+        padding: 5px;
+    }
+    .row{
+        margin-left: -4px;
+        margin-right: -4px;
+    }
+    .col-1{
+        padding-left: 4px;
+        padding-right: 4px;
+    }
+    .result-block{
+        margin-top: 20px;
+    }
+    .result-block__list {
+        column-count: 4;
+    }
+    .result-block__list button{
+        width: 100%;
+        margin-top: 10px;
+    }
 </style>
