d6047f8fa0a8888ea7f0297b0fb2a0b01c2c0d24
Dasha <dasha326@mail.ru>
2022-11-15 10:08:57 +0300

    resume

--- src/views/WordsView.vue
+++ src/views/WordsView.vue
@@ -5,14 +5,14 @@
         id="fieldset-1"
         label="Введите количество букв"
         label-for="count" >
-            <b-form-spinbutton id="count" v-model="letterCount" min="0" max="8" inline></b-form-spinbutton>
+            <b-form-spinbutton class="mt-3" id="count" v-model="letterCount" min="0" max="8" inline></b-form-spinbutton>
         </b-form-group>
         <b-row>
-            <b-col class="mt-3" cols="1" v-for="i in letterCount" :key="i">
-                <b-form-input width="20" name="letters"></b-form-input>
+            <b-col class="mt-3" cols="1" v-for="(item, i) in letterCount" :key="i">
+                <b-form-input width="20" size="lg" name="letters" v-model="currentLetters[i]" maxlength="1" ></b-form-input>
             </b-col>
         </b-row>
-        <b-button @click="findWords()">Поиск</b-button>
+        <b-button variant="primary" class="mt-3" @click="findWords()">Поиск</b-button>
 
         <b-list-group>
             <b-list-group-item v-for="word in currentWords" :key="word.id">{{word.name}}</b-list-group-item>
@@ -27,17 +27,39 @@
         data(){
             return{
                 currentWords: null,
-                letterCount: 0
+                letterCount: 0,
             }
         },
         methods: {
             findWords() {
+               // this.currentLetters.splice(0,this.currentLetters.length);
+                console.log(this.currentLetters)
                 this.currentWords = words.filter(el => el.name.length === this.letterCount)
+                console.log(this.currentWords.filter(elem => elem.name.indexOf(this.currentLetters) !== -1))
             }
-        }
+        },
+        computed: {
+            currentLetters(){
+                return Array.from({length: this.letterCount})
+            }
+            // inputState() {
+            //     return this.name.length > 2 ? true : false
+            // }
+        },
     }
 </script>
 
 <style scoped>
-
+.form-control{
+    text-align: center;
+    padding: 5px;
+}
+.row{
+    margin-left: -4px;
+    margin-right: -4px;
+}
+.col-1{
+    padding-left: 4px;
+    padding-right: 4px;
+}
 </style>
